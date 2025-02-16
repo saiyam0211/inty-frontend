ddced04a4f22521cfddeeac639ceda74c4aad2d4
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-02-16 22:45:43 +0530

    increace height

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -29,7 +29,7 @@ const Services = () => {
     <section className="py-12 px-6 md:px-12 lg:px-24 text-center  mb-12" >
       <h3 className="text-[#006452] text-lg font-semibold">Our Services</h3>
       <h2 className="text-3xl md:text-4xl font-bold mt-2">Find Whatever You Want</h2>
-      <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-4 lg:px-20 gap-4 mt-8">
+      <div className="grid grid-cols-1 px-10  sm:grid-cols-2 lg:grid-cols-4 lg:px-20 gap-4 mt-8">
         {services.map((service, index) => (
           <div key={index} className="bg-white shadow-md rounded-lg overflow-hidden"  data-aos="fade-up">
             <img src={service.image} alt={service.title} className="w-full h-45 object-cover" />
