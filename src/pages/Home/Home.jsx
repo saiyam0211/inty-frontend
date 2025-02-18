2eacc2ff3606d33f5e8049d033d14748158aa1bd
saiyam0211 <saiyamkumar2007@gmail.com>
2025-02-18 10:26:46 +0530

    Added InteriorPlatform

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import Hero from '../../components/Hero/Hero'
-import Services from '../../components/Services/Services'
-import Testimonials from '../../components/Testimonials/Testimonials'
-import OurPartners from '../../components/OurPartners/OurPartners'
-import Designers from '../../components/Designers/Designers'
-import Navbar from '../../components/Navbar/Navbar'
-import Features from '../../components/Features/Features'
-import Footer from '../../components/Footer/Footer'
-import Search from '../../components/Search/Search'
-import TestimonialCarousel from '../../components/TestimonialCarousel/TestimonialCarousel'
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import Services from "../../components/Services/Services";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import OurPartners from "../../components/OurPartners/OurPartners";
+import Designers from "../../components/Designers/Designers";
+import Navbar from "../../components/Navbar/Navbar";
+import Features from "../../components/Features/Features";
+import Footer from "../../components/Footer/Footer";
+import Search from "../../components/Search/Search";
+import TestimonialCarousel from "../../components/TestimonialCarousel/TestimonialCarousel";
+import InteriorPlatform from "../../components/InteriorPlatform/InteriorPlatform";
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <Services/>
-      <Search/>
-      <Hero/>
-      <Features/>
-      <Designers/>
-      <OurPartners/>
-      <TestimonialCarousel/>
-      <Footer/>
-        
+      <Navbar />
+      <Services />
+      <Search />
+      <InteriorPlatform />
+      <Hero />
+      <Features />
+      <Designers />
+      <OurPartners />
+      <TestimonialCarousel />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
